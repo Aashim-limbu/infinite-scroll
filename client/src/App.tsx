1ac27b87e814228ac4d0ac375bd26625b1ed038b
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-11-25 10:07:33 +0545

    finally finished the style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,11 +10,12 @@ type Photo = {
 	thumbnailUrl: string;
 };
 
-function App() {
+export default function App() {
 	const [photos, setPhotos] = useState<Photo[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const nextPhotoURLRef = useRef<string>();
-	const imageRef = useCallback((image : HTMLImageElement) => {
-        if (image == null) return;
+	const imageRef = useCallback((image: HTMLImageElement) => {
+		if (image == null || nextPhotoURLRef.current == null) return;
 		const observer = new IntersectionObserver((entries) => {
 			if (entries[0].isIntersecting) {
 				fetchPhotos(nextPhotoURLRef.current, { overwrite: false });
@@ -24,22 +25,29 @@ function App() {
 		observer.observe(image);
 	}, []);
 
-	function fetchPhotos(url?: string, { overwrite = false } = {}) {
+	async function fetchPhotos(url?: string, { overwrite = false } = {}) {
 		if (!url) return;
-		baseURL.get(url).then((res) => {
+		setIsLoading(true);
+		await new Promise((res) => setTimeout(res, 5000));
+		try {
+			const res = await baseURL.get(url);
 			if (res.headers == null) return;
 			if (typeof res.headers.get === "function") {
 				nextPhotoURLRef.current = parseLinkHeader(
 					res.headers.get("link")
 				)?.next;
-			}\
+			}
 			const photos = res.data;
-            console.log(photos)
+			console.log(photos);
 			if (overwrite) setPhotos(photos);
 			else {
 				setPhotos((p) => [...p, ...photos]);
 			}
-		});
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
 	}
 
 	useEffect(() => {
@@ -47,17 +55,26 @@ function App() {
 	}, []);
 
 	return (
-		<div className="grid p-2 mx-auto grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-4">
+		<div className="grid p-2 mx-auto  w-full grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-4">
 			{photos.map((photo, index) => (
 				<img
 					key={photo.id}
 					src={photo.url}
 					ref={photos.length - 1 == index ? imageRef : undefined}
-					className="mx-auto"
+					className="mx-auto flex aspect-square"
 				/>
 			))}
+			{isLoading &&
+				Array.from({ length: 10 }, (_, index) => index).map((n) => {
+					return (
+						<div
+							key={n}
+							className="bg-gray-300 aspect-square text-gray-400 animate-skeleton flex items-center justify-center text-6xl"
+						>
+							Loading...
+						</div>
+					);
+				})}
 		</div>
 	);
 }
-
-export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				skeleton: "skeleton 1s ease-in-out infinite alternate",
+			},
+			keyframes: {
+				skeleton: {
+					"0%": { opacity: "0.5" },
+					"100%": { opacity: "1" },
+				},
+			},
+		},
 	},
+	variants: {},
 	plugins: [],
 };
